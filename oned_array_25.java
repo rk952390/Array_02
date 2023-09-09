bd8a7aa890d2f2d0fe85d517a8d1634245750d74
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 11:35:24 +0530

    Add files via upload

---
+++ oned_array_25.java
@@ -0,0 +1,10 @@
+public class oned_array_25 {
+    public static void main(String args[])
+    {
+        int marks [] = {90,89,99};
+        //int [] marks={90,89,99};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
