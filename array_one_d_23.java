5136bccd2e86d2553f0fab50eafb6f42c00c6e14
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 11:34:07 +0530

    Add files via upload

---
+++ array_one_d_23.java
@@ -0,0 +1,22 @@
+public class array_one_d_23 {
+    public static void main(String args[])
+    {
+        int ages [] = { 10,20,30,40,50};
+        int length = ages.length;
+        int sum=0;
+
+       // System.out.println(length);
+
+        for(int i=0; i<=length; i++)
+        {
+            System.out.println(ages[i]);
+
+           // sum = sum + ages [i] ;
+
+        }  
+        System.out.println("sum of all arrays="+sum);
+        
+        
+
+    }
+}
