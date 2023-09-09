53817bf6fa6e786f50e06c2ed1a488461036d033
Rahul Kumar <142075106+rk952390@users.noreply.github.com>
2023-09-09 11:35:55 +0530

    Add files via upload

---
+++ oned_array_user_26.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class oned_array_user_26 {
+    public static void main(String args[])
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter the number=");
+            int num = sc.nextInt();
+            int size [] = new int[num];
+
+
+            for(int i=0;i<num;i++)
+            {
+                System.out.println(size[i]); //n time will be come to the zero
+
+            }
+        } 
+
+    }
+
+}
